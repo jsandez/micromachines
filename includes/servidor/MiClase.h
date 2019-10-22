cb01c327e2077940eaea0929ac658f8b9020ede2
Mateo Iván Calvo <mic@pop-os.localdomain>
2019-10-22 05:04:37 -0300

    Reorganizo fuentes

---
+++ includes/servidor/MiClase.h
@@ -0,0 +1,13 @@
+#ifndef _MICLASE_H_
+#define _MICLASE_H_
+
+class MiClase {
+private:
+    int attr1;
+    int attr2;
+public:
+    MiClase(int attr1, int attr2);
+    ~MiClase();
+};
+
+#endif
---
+++ src/servidor/MiClase.cpp
@@ -0,0 +1,9 @@
+#include "MiClase.h"
+
+MiClase::MiClase(int attr1, int attr2) :
+    attr1(attr1),
+    attr2(attr2){
+}
+
+MiClase::~MiClase() {
+}
