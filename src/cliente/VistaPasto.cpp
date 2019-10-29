6ec1c59be4fcc4de8cd44d22f2e152d9b23be820
Jorge Sandez <sandez.jorge@gmail.com>
2019-10-29 01:54:29 -0300

    camara

--- src/cliente/VistaPasto.cpp
+++ src/cliente/VistaPasto.cpp
@@ -4,7 +4,7 @@ VistaPasto::VistaPasto(int x, int y, int angulo, SdlTexture &sdl_texture) :
     VistaObjeto(x, y, angulo, sdl_texture) {}
 
 void VistaPasto::dibujar(int x, int y) {
-  Area src_area(0, 0, 4096, 2160);
-  Area dest_area(x, y, this->width, this->height);
+  Area src_area(x, y, 4096, 2160);
+  Area dest_area(0, 0, this->width, this->height);
   this->textura.render(src_area, dest_area, this->angulo, SDL_FLIP_NONE);
 }
