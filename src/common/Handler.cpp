d40d4d8c39cf037d6339ed401c22d1a4d8391f6b
Mateo <mateocalvo@gmail.com>
2019-10-30 04:25:52 -0300

    Agrego implementacion default vacia del Handler

---
+++ src/common/Handler.cpp
@@ -0,0 +1,10 @@
+#include "includes/common/Handler.h"
+
+void Handler::manejar(EventoAcelerar& e) {
+}
+
+void Handler::manejar(EventoCrearPartida& e) {
+}
+
+void Handler::manejar(EventoDesconexion& e) {
+}
