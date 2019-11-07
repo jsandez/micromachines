59fe44989388ba26095832ac0c3a3fe486f0af74
Mateo <mateocalvo@gmail.com>
2019-11-07 03:45:49 -0300

    Fixes y desarrollo en partida

--- includes/servidor/Partida.h
+++ includes/servidor/Partida.h
@@ -3,21 +3,23 @@
 
 #include "includes/common/Hilo.h"
 #include "includes/common/Handler.h"
-#include "includes/common/ColaProtegida.h"
+#include "includes/common/Cola.h"
 #include "includes/servidor/Jugador.h"
 #include "includes/servidor/modelo/Mundo.h"
 
 #include <map>
 #include <memory>
 
-class Partida : public Hilo, public Handler {
+class Partida : public Hilo {
 private:
     std::map<uint32_t, std::shared_ptr<Jugador>> jugadores_;
-    ColaProtegida<std::shared_ptr<Evento>> eventosEntrantes_;
+    Cola<std::shared_ptr<Evento>> eventosEntrantes_;
     Mundo mundo_;
 
     Partida(const Partida& otra) = delete;
     Partida& operator=(const Partida& otra) = delete;
+
+    void step(uint32_t iteracion);
 
 public:
     Partida(uint16_t uuidPista);
--- src/servidor/Partida.cpp
+++ src/servidor/Partida.cpp
@@ -1,5 +1,13 @@
 #include "includes/servidor/Partida.h"
 
+#include <cmath>
+#include <thread>
+#include <chrono>
+
+#include "includes/common/Cronometro.h"
+#include "includes/common/Cola.h"
+#include "includes/servidor/utils/ConfigServidor.h"
+
 Partida::Partida(uint16_t uuidPista) :
     mundo_(uuidPista) {
 }
@@ -11,16 +19,42 @@ void Partida::agregarJugador(std::shared_ptr<Jugador> jugador) {
     jugadores_[jugador->uuid()] = jugador;
 }
 
+void Partida::step(uint32_t nroIteracion) {
+    bool obtenido = false;
+    std::shared_ptr<Evento> evento;
+    while(obtenido = eventosEntrantes_.get(evento)) {
+        manejar(*evento);
+    }
+    mundo_.step(nroIteracion);
+    Cola<std::shared_ptr<Evento>>& eventosOcurridos = mundo_.eventosOcurridos();
+    std::shared_ptr<Evento> eventoOcurrido;
+    while(obtenido = eventosOcurridos.get(eventoOcurrido)) {
+        for (auto& kv : jugadores_) {
+            kv.second->manejar(*eventoOcurrido);
+        }
+    }
+}
+
 void Partida::correr() {
-    //TODO: implementar Partida::correr()
     // Asignar un auto a cada jugador presente, no poner autos vacios
-    //bool obtenido;
-    //std::shared_ptr<Evento> evento;
+    double frecuencia = 1 / CONFIG_SERVIDOR.iteracionesPorSegundo();
+    Cronometro c;
+    double t1 = c.ahora();
+    uint32_t iteracion = 0;
     while(seguirCorriendo_) {
-        // Tomar eventos
-        // Aplicarlos
-        // Step()
-        // para cada jugador, enviarle estado del juego
+        step(iteracion);
+        double t2 = c.ahora();
+        double resto = frecuencia - (t2 - t1);
+        if (resto < 0) {
+            double atraso = -resto;
+            double perdidos = atraso - std::fmod(atraso, frecuencia);
+            resto = frecuencia - std::fmod(atraso, frecuencia);
+            t1 += perdidos;
+            iteracion += std::floor(perdidos / frecuencia);
+        }
+        dormir(resto);
+        t1 += frecuencia;
+        iteracion += 1;        
     }
 }
 
@@ -33,6 +67,7 @@ void Partida::ocurrio(std::shared_ptr<Evento> unEvento) {
 }
 
 void Partida::manejar(Evento& e) {
+    mundo_.manejar(e);
     e.actualizar(*this);
 }
 
