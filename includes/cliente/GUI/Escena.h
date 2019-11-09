b4e22955880c464730e12dae4374a459e265a1c8
Mateo Ivan Calvo <macalvo@gi.uba.ar>
2019-11-09 09:40:57 -0300

    Quito constructor de escena, era vacio

--- includes/cliente/GUI/Escena.h
+++ includes/cliente/GUI/Escena.h
@@ -1,7 +1,6 @@
 #ifndef _ESCENA_H_
 #define _ESCENA_H_
 
-//#include "includes/cliente/GUI/Ventana.h"
 #include "includes/cliente/GUI/Renderizador.h"
 #include "includes/cliente/GUI/Textura.h"
 
@@ -9,8 +8,6 @@ class Escena {
 private:
 
 public:
-    Escena();
-
     Textura dibujate(uint32_t numeroIteracion, Renderizador& renderizador, Area dimensiones);
 };
 
