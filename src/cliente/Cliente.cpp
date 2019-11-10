83d4dca8d7038880fc77f04996176d57e0b31b4e
Mateo Ivan Calvo <macalvo@gi.uba.ar>
2019-11-10 13:22:21 -0300

    ASD

--- src/cliente/Cliente.cpp
+++ src/cliente/Cliente.cpp
@@ -1,6 +1,7 @@
 #include "includes/cliente/Cliente.h"
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 Cliente::Cliente(unsigned int anchoVentana, unsigned int altoVentana, bool pantallaCompleta, const std::string& tituloVentana, const std::string& host, const std::string& puerto) :
     ventana_(anchoVentana, altoVentana, pantallaCompleta, tituloVentana),
@@ -25,6 +26,63 @@ void Cliente::correr() {
 
     recibidor_.iniciar();
     dibujador_.iniciar();
+
+    SDL_Event evento;
+    while (SDL_WaitEvent(&evento) != 0) {
+        switch (evento.type) {
+        case SDL_KEYDOWN:
+            manejarKeyDown();
+
+            
+         {
+            SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent &)evento;
+            switch (keyEvent.keysym.sym) {
+                            case SDLK_LEFT:
+                                x -= 10;
+                                break;
+                            case SDLK_RIGHT:
+                                x += 10;
+                                break;
+                            case SDLK_UP:
+                                y -= 10;
+                                break;
+                            case SDLK_DOWN:
+                                y += 10;
+                                break;
+                            }
+                    } // Fin KEY_DOWN
+        }
+    }
+    while (SDL_WaitEvent(&evento) != 0) {
+        x = 0;
+        y = 0;
+        switch (event.type) {
+          case SDL_KEYDOWN: {
+            
+            if (event.key.repeat == 0) {
+              switch (keyEvent.keysym.sym) {
+                case SDLK_SPACE: {
+                  std::shared_ptr<Evento> eventoAcelerar = std::make_shared<EventoAcelerar>();
+                  cola_bloqueante.put(eventoAcelerar);
+                  break;
+                }
+              }
+            }
+          }
+          case SDL_MOUSEMOTION:std::cout << "Oh! Mouse" << std::endl;
+            break;
+          case SDL_QUIT:std::cout << "Quit :(" << std::endl;
+            running = false;
+            break;
+        }
+      }
+
+
+
+
+
+
+
 
     char c;
     while ((c = std::cin.get()) != 'q') {
