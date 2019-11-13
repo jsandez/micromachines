fc919632743abea52cc56bf06ff2fc7bbd35be9d
Mateo <mateocalvo@gmail.com>
2019-11-13 11:04:21 -0300

    Evento id jugador

--- src/common/red/Protocolo.cpp
+++ src/common/red/Protocolo.cpp
@@ -10,7 +10,18 @@ uint16_t Protocolo::recibirNumUnsigned16() {
     return ntohs(resultadoRed);
 }
 
+uint32_t Protocolo::recibirNumUnsigned32() {
+    uint32_t resultadoRed = 0;
+    socket_.recibirN((char*) &resultadoRed, LEN_32);
+    return ntohl(resultadoRed);
+}
+
 void Protocolo::enviar(uint16_t unNumero) {
     uint16_t numeroRed = htons(unNumero);
     socket_.enviarN((const char*) &numeroRed, LEN_16);
+}
+
+void Protocolo::enviar(uint32_t unNumero) {
+    uint32_t numeroRed = htonl(unNumero);
+    socket_.enviarN((const char*) &numeroRed, LEN_32);
 }
