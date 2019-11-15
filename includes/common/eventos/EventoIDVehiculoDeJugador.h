2cdce48aac2d2c7e0f75a6a66ab0fe75a185d5b8
Mateo Iván Calvo <macalvo@fi.uba.ar>
2019-11-15 01:25:31 -0300

    Cambio evento que representa el id del vehiculo de cada jguador a algo más descriptivo

--- includes/common/eventos/EventoIDVehiculoDeJugador.h
+++ includes/common/eventos/EventoIDVehiculoDeJugador.h
@@ -4,12 +4,12 @@
 #include "includes/common/eventos/Evento.h"
 #include "includes/common/red/Protocolo.h"
 
-class EventoIDJugador : public Evento {
+class EventoIDVehiculoDeJugador : public Evento {
 public:
-    uint32_t uuidJugador_;
+    uint8_t idDelVehiculo_;
     
-    EventoIDJugador(uint32_t uuidRemitente, Protocolo& protocolo);
-    EventoIDJugador(uint32_t uuidJugador);
+    EventoIDVehiculoDeJugador(uint32_t uuidRemitente, Protocolo& protocolo);
+    EventoIDVehiculoDeJugador(uint8_t idDelVehiculo);
     virtual void enviarse(Protocolo& protocolo) override;
     virtual void actualizar(Handler& handler) override;
 };
