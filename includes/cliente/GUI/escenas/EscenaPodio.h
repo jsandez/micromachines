6ae78cd80623280c2a446de7a54a5e83cef5246f
Jorge Sandez <sandez.jorge@gmail.com>
2019-11-25 23:35:22 -0300

    boton cpu

--- includes/cliente/GUI/escenas/EscenaPodio.h
+++ includes/cliente/GUI/escenas/EscenaPodio.h
@@ -1,19 +1,15 @@
-#ifndef _ESCENA_LOBBY_H_
-#define _ESCENA_LOBBY_H_
+#ifndef _ESCENA_PODIO_H_
+#define _ESCENA_PODIO_H_
 
-#include <includes/common/ColaProtegida.h>
 #include "includes/cliente/GUI/escenas/Escena.h"
-#include "includes/cliente/GUI/Boton.h"
 
-class EscenaLobby : public Escena {
- private:
+class EscenaPodio {//: public Escena {
+/* private:
   Animacion fondoMenu_;
   ColaProtegida<std::shared_ptr<EventoGUI>> &eventosGUI_;
   std::map<int, std::shared_ptr<Boton>> botones;
   std::map<int, std::shared_ptr<Texto>> textoJugadores;
-  std::map<int,uint32_t> jugadoresId;
-  std::map<int,bool> jugadoresEstaListo;
-  bool cpu;
+  std::map<int, uint32_t> jugadoresId;
 
   void inicializarBotones();
   void inicializarTextoJugadores();
@@ -21,26 +17,21 @@ class EscenaLobby : public Escena {
   void handlerBotones(int uuid);
   void dibujarTextoJugadores(int iteracion);
  public:
-  EscenaLobby(Renderizador &renderizador,
+  EscenaPodio(Renderizador &renderizador,
               ColaProtegida<std::shared_ptr<EventoGUI>> &eventosGUI,
               std::stack<std::shared_ptr<Escena>> &escenas,
               ColaBloqueante<std::shared_ptr<Evento>> &eventosAEnviar_,
               Sonido &musicaAmbiente,
-              EventoPartidaCreada& e);
-
-  EscenaLobby(Renderizador &renderizador,
-              ColaProtegida<std::shared_ptr<EventoGUI>> &eventosGUI,
-              std::stack<std::shared_ptr<Escena>> &escenas,
-              ColaBloqueante<std::shared_ptr<Evento>> &eventosAEnviar_,
-              Sonido &musicaAmbiente,
-              EventoSnapshotLobby& e);
+              EventoSnapshotSala &e);
   virtual Textura dibujate(uint32_t numeroIteracion, Area dimensiones) override;
   virtual void manejarInput(EventoGUI &evento) override;
   virtual void manejarInput(EventoGUIClick &evento) override;
   virtual void manejarInput(EventoGUIKeyDown &evento) override;
   virtual void manejarInput(EventoGUIKeyUp &evento) override;
-  virtual void manejar(Evento &e) override;
-  virtual void manejar(EventoPartidaIniciada &estadoInicial) override;
-  virtual void manejar(EventoSnapshotLobby &e) override;
+
+ virtual void manejar(Evento &e) override;
+  virtual void manejar(EventoSnapshotSala &e) override;
+  virtual void manejar(EventoPartidaCreada &e) override;
+  virtual void manejar(EventoSnapshotLobby &e) override;*/
 };
 #endif
---
+++ src/cliente/GUI/escenas/EscenaPodio.cpp
@@ -0,0 +1,1 @@
+#include "includes/cliente/GUI/escenas/EscenaPodio.h"
